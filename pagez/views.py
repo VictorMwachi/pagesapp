e1ea311e29987b53d831bbbc33623832e1607fe1
Victor Mudaki Mwachi <mudaki@localhost.localdomain>
2020-05-14 12:42:24 -0400

    initial commit

---
+++ pagez/views.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.http import HttpResponse
+# Create your views here.
+
+class HomePageView(TemplateView):
+    template_name='home.html'
+
+class AboutPageView(TemplateView):
+    template_name='about.html'
